ce2f55be8227951ecfda176906be0a54c0ae7bed
Kirill Khudyakov <khudiakovk@gmail.com>
2023-04-26 14:36:44 +0700

    add subscription manager

---
+++ app/src/main/java/com/khudyakov/carstore/data/subscription/SubscriptionManager.kt
@@ -0,0 +1,16 @@
+package com.khudyakov.carstore.data.subscription
+
+class SubscriptionManager {
+
+    suspend fun isSubscribed(): Boolean {
+        TODO()
+    }
+
+    suspend fun canShow(): Boolean {
+        TODO()
+    }
+
+    suspend fun canDownload(): Boolean {
+        TODO()
+    }
+}
