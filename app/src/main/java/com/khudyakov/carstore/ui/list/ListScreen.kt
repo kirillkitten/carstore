e8cb7584ce0555edae3cd0a23deadfa9f26a4b4d
Kirill Khudyakov <khudiakovk@gmail.com>
2023-04-28 08:19:10 +0700

    add top bar and fab

--- app/src/main/java/com/khudyakov/carstore/ui/list/ListScreen.kt
+++ app/src/main/java/com/khudyakov/carstore/ui/list/ListScreen.kt
@@ -1,24 +1,67 @@
 package com.khudyakov.carstore.ui.list
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.twotone.DirectionsCar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.khudyakov.carstore.data.car.Car
 
 @Composable
 fun ListScreen(viewModel: ListViewModel) {
     val cars = viewModel.cars.collectAsState(initial = emptyList())
-    ListScreen(cars = cars.value, onCarAdded = viewModel::addCar)
+    ListScreen(cars = cars.value, onAddClicked = {})
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListScreen(cars: List<Car>, onCarAdded: (Car) -> Unit) {
-    LazyVerticalGrid(columns = GridCells.Fixed(count = 2)) {
-        items(cars) { car ->
-            CarItem(car = car)
+fun ListScreen(cars: List<Car>, onAddClicked: () -> Unit) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Icon(imageVector = Icons.TwoTone.DirectionsCar, contentDescription = null)
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddClicked, shape = CircleShape) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) { padding ->
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(count = 2),
+            modifier = Modifier.padding(padding)
+        ) {
+            items(cars) { car ->
+                CarItem(car = car)
+            }
         }
     }
 }
@@ -33,5 +76,5 @@ fun ListScreenPreview() {
         Car("Lada", "", 2011, 1.4, time - 200000000L),
         Car("Volkswagen", "", 2020, 1.6, time - 300000000L)
     )
-    ListScreen(cars = cars, onCarAdded = {})
+    ListScreen(cars = cars, onAddClicked = {})
 }
