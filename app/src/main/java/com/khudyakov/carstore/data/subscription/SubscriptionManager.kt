ade95c29648c595db55a18e9937318e4c565f030
Kirill Khudyakov <khudiakovk@gmail.com>
2023-05-09 23:36:06 +0700

    start to implement subscription manager

--- app/src/main/java/com/khudyakov/carstore/data/subscription/SubscriptionManager.kt
+++ app/src/main/java/com/khudyakov/carstore/data/subscription/SubscriptionManager.kt
@@ -1,16 +1,53 @@
 package com.khudyakov.carstore.data.subscription
 
-class SubscriptionManager {
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
 
-    suspend fun isSubscribed(): Boolean {
-        TODO()
+val Context.datastore by preferencesDataStore("settings")
+
+@Singleton
+class SubscriptionManager @Inject constructor(@ApplicationContext context: Context) {
+
+    private val dataStore = context.datastore
+
+    private val freeShowCount = 3
+    private val freeUploadCount = 2
+
+    private val subscribedKey = booleanPreferencesKey("subscribed")
+    private val showCountKey = intPreferencesKey("shows")
+    private val uploadCountKey = intPreferencesKey("uploads")
+
+    fun isSubscribed(): Flow<Boolean> = dataStore.data.map { preferences ->
+        preferences[subscribedKey] ?: false
     }
 
-    suspend fun canShow(): Boolean {
-        TODO()
+    fun canShow(): Flow<Boolean> = dataStore.data.map { preferences ->
+        (preferences[showCountKey] ?: 0) < freeShowCount
     }
 
-    suspend fun canDownload(): Boolean {
-        TODO()
+    fun canUpload(): Flow<Boolean> = dataStore.data.map { preferences ->
+        (preferences[uploadCountKey] ?: 0) < freeUploadCount
     }
+
+    suspend fun addShow() {
+        dataStore.edit { settings ->
+            settings[showCountKey] = (settings[showCountKey] ?: 0) + 1
+        }
+    }
+
+    suspend fun addUpload() {
+        dataStore.edit { settings ->
+            settings[uploadCountKey] = (settings[uploadCountKey] ?: 0) + 1
+        }
+    }
+
+    enum class Status { NotSubscribed, Trial, Subscribed }
 }
